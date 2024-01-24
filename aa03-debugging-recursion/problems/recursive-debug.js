3cd35feb76379b9f87e5378e90f6c55593cf56c6
Shelbey Casalena <shelbey.casalena@gmail.com>
2024-01-24 15:05:50 -0800

    whole thing

---
+++ aa03-debugging-recursion/problems/recursive-debug.js
@@ -0,0 +1,29 @@
+/***********************************************************************
+We've done most of the work for you below, but something's not quite
+right. Refactor the problem below to avoid a stack overflow and save
+the day!
+
+When fixed, this function should behave similarly to JavaScript's '.map()'
+function.
+
+Examples:
+
+doForAll([], (x) => x * 2); // => []
+doForAll([1, 2, 3], (x) => x + 1); // => [2, 3, 4]
+doForAll(["a", "b", "c"], (x) => x.toUpperCase()); // => ["A", "B", "C"]
+***********************************************************************/
+
+function doForAll(arr, action) {
+  if (arr.length <= 0) {
+    return [];
+  }
+  let changedEle = action(arr[0]);
+  arr.shift();
+  return [changedEle].concat(doForAll(arr, action));
+}
+debugger;
+console.log(doForAll([], (x) => x * 2)); // => []
+console.log(doForAll([1, 2, 3], (x) => x + 1)); // => [2, 3, 4]
+doForAll(["a", "b", "c"], (x) => x.toUpperCase()); // => ["A", "B", "C"]
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+module.exports = doForAll;
